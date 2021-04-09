7bb54cf0e441bf82b42240ba8f5f17dd8d312b1f
Kyle M Hall <kyle@bywatersolutions.com>
2021-04-09 10:20:34 -0400

    Re-add debugging

---
+++ docs/js/header.js
@@ -0,0 +1,14 @@
+(function () {
+  var form = document.getElementById('header-search-form');
+  var input = document.getElementById('header-search-input');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var text = input.value.trim();
+    if (text === '') {
+      return;
+    }
+    var googleSearchPrefix = 'https://www.google.com/#newwindow=1&q=';
+    var siteUrl = window.HEXO_DATA.config.url;
+    window.open(googleSearchPrefix + 'site:' + siteUrl + '+' + text, '_blank');
+  });
+}());
